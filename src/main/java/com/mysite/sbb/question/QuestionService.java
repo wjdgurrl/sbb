8a79400d363d9f610b1d0474384c2248a007b7ff
gurrl <gurrl8356@gmail.com>
2023-12-05 23:22:32 +0900

    Initial commit

---
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -0,0 +1,44 @@
+package com.mysite.sbb.question;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import com.mysite.sbb.DataNotFoundException;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList(){
+        return this.questionRepository.findAll();
+    }
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        }
+        else{
+            throw new DataNotFoundException("question not found");
+        }
+    }
+    public void create(String subject, String content){
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
+    }
+    public Page<Question> getList(int page){
+        Pageable pageable = PageRequest.of(page,10);
+        return this.questionRepository.findAll(pageable);
+    }
+}
